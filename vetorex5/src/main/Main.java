52e21c564c640c7501bc72cf3ff35718a847809b
caiocardoso418 <caiocardoso418@gmail.com>
2026-06-11 22:18:19 -0300

    exercicio concluido vetor maior pelo udemy

---
+++ vetorex5/src/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Quantos numeros voce vai digitar: ");
+		int n = sc.nextInt();
+		
+		
+		int posmaior;
+		double maior;
+		int[] vetor = new int[n];
+		
+		for(int i=0; i<vetor.length; i++) {
+			System.out.print("Digite um numero: ");
+			vetor[i] = sc.nextInt();
+		}
+		
+		maior = vetor[0];
+	    posmaior = 0;
+
+	    for (int i=1; i<vetor.length; i++) {
+	        if (vetor[i] > maior) {
+	            maior = vetor[i];
+	            posmaior = i;
+	        }
+	    }
+		
+	    System.out.printf("MAIOR VALOR = %.1f\n", maior);
+	    System.out.printf("POSICAO DO MAIOR VALOR = %d\n", posmaior+1);
+
+		
+		
+		sc.close();
+	}
+}
